89ee0a2789b55eb1dbb363ca6c3a8577e586c934
jbugakova <bugakovayk@gmail.com>
2020-05-16 22:41:15 +0300

    Implemented fixed navigation panel and burger menu.

--- src/app.js
+++ src/app.js
@@ -4,7 +4,15 @@ import './styles.scss';
 
 import 'owl.carousel/dist/owl.carousel.min.js';
 
-$(document).ready(function(){
+$(document).ready(function() {
+    const STICKED_CLASS = 'sticked';
+    const $menuButton = $('#menuButton');
+    const $navPanel = $('#navPanel');
+    const $navMenu = $('#navMenu');
+    let navMenuIsOpen = false;
+
+    checkScroll();
+
     $('.home-owl-carousel').owlCarousel({
         items: 1,
         dots: true,
@@ -23,15 +31,55 @@ $(document).ready(function(){
         nav: true,
         dots: false
     });
+
+    $(window).on('scroll', checkScroll);
+    $('#playVideoBtn').on('click', onPlayVideoBtnClick);
+    $menuButton.on('click', onMenuButtonClick);
+
+    function onPlayVideoBtnClick() {
+        $('#videoCover').fadeOut(1000, function() {
+            $(this).remove();
+            const $videoPlayer = $('#videoPlayer');
+            $videoPlayer.show();
+            $videoPlayer.find('iframe').attr('src', 'https://www.youtube.com/embed/BztJ2iKrCGY?autoplay=1');
+        });
+    }
+    function onMenuButtonClick() {
+        if(!navMenuIsOpen) {
+            openMenu();
+        } else {
+            closeMenu();
+        }
+    }
+    function closeMenu() {
+        navMenuIsOpen = false;
+        $menuButton.removeClass('open');
+        $navMenu.removeClass('opened-menu');
+
+        if($navPanel.hasClass(STICKED_CLASS) && !$navMenu.hasClass('opened-menu') && isScrollOnTop()) {
+            $navPanel.removeClass(STICKED_CLASS)
+        }
+    }
+    function openMenu() {
+        navMenuIsOpen = true;
+        $menuButton.addClass('open');
+        $navMenu.addClass('opened-menu');
+
+        if(!$navPanel.hasClass(STICKED_CLASS)) {
+            stickNavPanel();
+        }
+    }
+    function checkScroll() {
+        if(!isScrollOnTop()) {
+            stickNavPanel();
+        } else {
+            $navPanel.removeClass(STICKED_CLASS);
+        }
+    }
+    function isScrollOnTop() {
+        return $(window).scrollTop() === 0;
+    }
+    function stickNavPanel() {
+        $navPanel.addClass(STICKED_CLASS);
+    }
 });
-
-$('#playVideoBtn').on('click', onPlayVideoBtnClick);
-
-function onPlayVideoBtnClick() {
-    $('#videoCover').fadeOut(1000, function() {
-        $(this).remove();
-        const $videoPlayer = $('#videoPlayer');
-        $videoPlayer.show();
-        $videoPlayer.find('iframe').attr('src', 'https://www.youtube.com/embed/BztJ2iKrCGY?autoplay=1');
-    });
-}
