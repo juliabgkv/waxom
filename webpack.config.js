ed1f8985f64610642d92e9f16121e65ffd3b2191
jbugakova <bugakovayk@gmail.com>
2020-05-20 19:38:26 +0300

    Added autoprefixer.

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,9 @@ const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const autoprefixer = require('autoprefixer');
 
-module.exports ={
+module.exports = {
     mode: 'development',
     devtool: 'inline-source-map',
     devServer: {
@@ -23,10 +24,33 @@ module.exports ={
             {
                 test: /\.scss$/,
                 use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ]
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: [
+                                autoprefixer({
+                                    browsers:['ie >= 8', 'last 4 version']
+                                })
+                            ],
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    }
+                ],
             },
             {
                 test: /\.css$/i,
@@ -46,7 +70,8 @@ module.exports ={
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: './src/index.html'
+            template: './src/index.html',
+            inject: 'head'
         }),
         new CleanWebpackPlugin(),
         new webpack.ProvidePlugin({
